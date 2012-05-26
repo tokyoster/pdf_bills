e509d4a01575f3046a6cc09e3c679e3822f28100
Yoshiteru Toki <take.it.easy.ggl@gmail.com>
2012-05-26 16:41:37 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdfBills::Application.config.secret_token = 'ab78e86057f3fcd658a9aee9b99997209424174b348db97aca696368e27f9a17424c1faa5dfc4141cbd3ae145c64f99da1bd691d6cdfe7ef16407fcdd3e74d60'
